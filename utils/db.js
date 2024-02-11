4d9771a87b19224e935fc595b2dca24cf57e14a3
Keith koki <kokikeith@gmail.com>
2024-02-11 00:56:37 -0800

    first commit

--- utils/db.js
+++ utils/db.js
@@ -3,37 +3,36 @@ import { MongoClient } from 'mongodb';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
-    constructor() {
-        this.client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}/`, { useNewUrlParser: true, useUnifiedTopology: true });
-    }
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
+  }
 
-    async connect() {
-        try {
-            await this.client.connect();
-            this.db = this.client.db(DB_DATABASE);
-        } catch (err) {
-            console.error('Error connecting to MongoDB:', err);
-            this.client.close();
-        }
-    }
+  isAlive() {
+    return Boolean(this.db);
+  }
 
-    isAlive() {
-        return this.client.isConnected();
-    }
+  async nbUsers() {
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
+  }
 
-    async nbUsers() {
-        const usersCollection = this.db.collection('users');
-        const count = await usersCollection.countDocuments();
-        return count;
-    }
-
-    async nbFiles() {
-        const filesCollection = this.db.collection('files');
-        const count = await filesCollection.countDocuments();
-        return count;
-    }
+   async nbFiles() {
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
+  }
 }
 
 const dbClient = new DBClient();

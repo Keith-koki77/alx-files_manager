603485ae350bbf0a39752cda7a55af00bdbd1e1b
Keith koki <kokikeith@gmail.com>
2024-02-14 11:59:29 -0800

    file updated

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,44 @@
-import express from 'express';
-import UsersController from '../controllers/UsersController';
+import { Router } from 'express';
+
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+import UtilController from '../controllers/UtilController';
 
+const router = Router();
 
-const router = express.Router();
-
-router.get('/status', async (req, res) => {
-  try {
-    const status = await AppController.getStatus();
-    res.status(200).json(status);
-  } catch (error) {
-    console.error('Error getting status:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+router.use((request, response, next) => {
+  const paths = ['/connect'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers.authorization) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
   }
 });
 
-router.get('/stats', async (req, res) => {
-  try {
-    const stats = await AppController.getStats();
-    res.status(200).json(stats);
-  } catch (error) {
-    console.error('Error getting stats:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+router.use((request, response, next) => {
+  const paths = ['/disconnect', '/users/me', '/files'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers['x-token']) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
   }
 });
 
-router.post('/users', async (req, res) => {
-  try {
-    const newUser = await UsersController.postNew(req, res);
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
-export default router;
+router.put('/files/:id/publish', UtilController.token, FilesController.putPublish);
+router.put('/files/:id/unpublish', UtilController.token, FilesController.putUnpublish);
+module.exports = router;

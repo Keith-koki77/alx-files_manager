a5d82826ad055f9d1382bfd2d1e0e0c9ac7c8a27
Keith koki <kokikeith@gmail.com>
2024-02-14 01:05:07 -0800

    file fixed

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,72 +1,33 @@
-import { ObjectId } from 'mongodb';
-import bcrypt from 'bcrypt';
-import Queue from 'bull';
-import dbClient from '../utils/db';
-import userUtils from '../utils/user';
+import DBClient from '../utils/db.js';
+import { hashPassword } from '../utils/auth.js'; 
 
-const userQueue = new Queue('userQueue');
+async function postNew(req, res) {
 
-class UsersController {
-  static async postNew(request, response) {
-    const { email, password } = request.body;
+  const { email, password } = req.body;
 
-    if (!email) return response.status(400).send({ error: 'Missing email' });
-
-    if (!password) { return response.status(400).send({ error: 'Missing password' }); }
-
-    const emailExists = await dbClient.usersCollection.findOne({ email });
-
-    if (emailExists) { return response.status(400).send({ error: 'Already exist' }); }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    let result;
-    try {
-      result = await dbClient.usersCollection.insertOne({
-        email,
-        password: hashedPassword,
-      });
-    } catch (err) {
-      await userQueue.add({});
-      return response.status(500).send({ error: 'Error creating user.' });
-    }
-
-    const user = {
-      id: result.insertedId,
-      email,
-    };
-
-    await userQueue.add({
-      userId: result.insertedId.toString(),
-    });
-
-    return response.status(201).send(user);
+  if (!email) {
+    return res.status(400).json({"error": "Missing email"});
   }
 
-  /**
-   *
-   * Should retrieve the user base on the token used
-   *
-   * Retrieve the user based on the token:
-   * If not found, return an error Unauthorized with a
-   * status code 401
-   * Otherwise, return the user object (email and id only)
-   */
-  static async getMe(request, response) {
-    const { userId } = await userUtils.getUserIdAndKey(request);
-
-    const user = await userUtils.getUser({
-      _id: ObjectId(userId),
-    });
-
-    if (!user) return response.status(401).send({ error: 'Unauthorized' });
-
-    const processedUser = { id: user._id, ...user };
-    delete processedUser._id;
-    delete processedUser.password;
-
-    return response.status(200).send(processedUser);
+  if(!password) {
+    return res.status(400).json({"error": "Missing password"}); 
   }
+
+  const user = await DBClient.findUserByEmail(email);
+  
+  if (user) {
+    return res.status(400).json({"error": "Already exists"});  
+  }
+
+  const hashedPassword = hashPassword(password);
+
+  const newUser = await DBClient.insertUser(email, hashedPassword);
+  
+  return res.status(201).json({
+    id: newUser._id,
+    email: newUser.email
+  });
+
 }
 
-export default UsersController;
+export default { postNew };
